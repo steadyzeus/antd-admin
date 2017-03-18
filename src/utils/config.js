6d9f948722d265baac6a9e63d7f2f6a52875680c
zhouyunlong <zhouyunlong@cd.tuan800.com>
2017-03-18 21:59:51 +0800

    提交代码

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: '中标贷信息录入系统',
   prefix: 'zbAdmin',
-  footerText: '中标贷信息录入系统 版权所有 © 2017 由 steady 支持',
+  footerText: '中标贷信息录入系统 版权所有 © 2017 由 成都爱拓网络科技有限公司 支持',
   logoSrc: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
   logoText: '中标贷信息录入系统',
   needLogin: true
